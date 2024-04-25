5b7600de9812b69c788f6b0a3e3fd21e563c20b7
yanyue <yanyue171@mails.ucas.ac.cn>
2024-04-25 15:10:09 +0800

    适配jekyll

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -15,10 +15,12 @@ then
     usage
 fi
 
+# realpath 获取绝对路径，dirname 获取目录名，$0 为当前脚本的路径
 TEMPLATE="$(dirname $(realpath $0))/template.html"
 HEAD="$(dirname $(realpath $0))/head.html"
-MD="$1"
-HTML="${1%.*}.html"
+MD="$1"     # 输入的 markdown 文件
+HTML="${1%.*}.html"     # 输出的 html 文件, 去掉后缀
+FRONTMATTER="$(dirname $(realpath $0))/frontmatter.txt"  # 前置元数据文件的路径
 
 pandoc \
     --metadata pagetitle="Markdown Cheatsheet" \
@@ -26,5 +28,11 @@ pandoc \
     --toc \
     --template="${TEMPLATE}" \
     -H "${HEAD}" \
-    -o "${HTML}" \
+    -o "temp.html" \
     "${MD}"
+
+# 将前置元数据和生成的 HTML 文件合并到最终的 HTML 文件
+cat "${FRONTMATTER}" temp.html > "${HTML}"
+
+# 清理临时文件
+rm temp.html 
